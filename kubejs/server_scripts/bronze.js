b1fb517e741d979b43d94c26de038a76a1f8593e
Sam <samcashlamore@gmail.com>
2024-01-25 03:50:21 -0500

    A few basic quest

--- kubejs/server_scripts/bronze.js
+++ kubejs/server_scripts/bronze.js
@@ -1,4 +1,5 @@
 ServerEvents.recipes(event => {
     // remove indrev bronze
     //removeIndrevMaterial(event, "bronze", true, true)
+    createPlateRecipes(event, "modern_industrialization:bronze_ingot", "modern_industrialization:bronze_plate", 200, false, false)
 })
